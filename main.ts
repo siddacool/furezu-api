df45506e631e393fd3eb0b945aed0dd1335f9a0e
Siddhesh Mangela <sidthecool007@gmail.com>
2024-10-01 15:49:25 +0530

    caching added

--- main.ts
+++ main.ts
@@ -1,18 +1,39 @@
 import { Application } from 'jsr:@oak/oak/application';
 import { Router } from 'jsr:@oak/oak/router';
 import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
-import { getLibrary } from './data/library/index.ts';
+import { LibraryData } from './data/library/index.ts';
 
 const router = new Router();
 router
   .get('/', oakCors(), (context) => {
-    context.response.body = 'Hello world!';
+    context.response.body = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Document</title>
+        </head>
+        <body>
+          <h1>Furezu API</h1>
+          <ul>
+            <li><a href="library">Library</a></li>
+          </ul>
+        </body>
+      </html>
+    `;
   })
   .options('/library', oakCors())
   .get('/library', oakCors(), async (context) => {
-    const data = await getLibrary('./data/library/books');
+    const libraryFile = await Deno.readTextFile('library.json');
+    const data = JSON.parse(libraryFile) as LibraryData[];
 
     context.response.body = data;
+  }).options('/library-cache-version', oakCors())
+  .get('/library-cache-version', oakCors(), async (context) => {
+    const libraryFileVersion = await Deno.readTextFile('library.version.txt');
+
+    context.response.body = libraryFileVersion;
   });
 
 const app = new Application();
