b368b88c845b3e84bce90db4f1d00f05cdd1f0b1
Siddhesh Mangela <sidthecool007@gmail.com>
2024-10-01 14:42:55 +0530

    fix

---
+++ data/library/index.ts
@@ -0,0 +1,57 @@
+export interface Phrase {
+  id?: number;
+  _id: string;
+  meaning: string;
+  phrase: string;
+  pronounciation?: string;
+  translation?: string;
+  bookId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  hidden?: boolean;
+  importedAt?: Date;
+}
+
+export interface Book {
+  id?: number;
+  _id: string;
+  name: string;
+  voice?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  importedAt?: Date;
+}
+
+export type LibraryData = {
+  _id: string;
+  book: Book;
+  phrases: Phrase[];
+  exportedAt: Date;
+};
+
+export async function getLibrary(path: string) {
+  try {
+    const library = [];
+
+    for await (const dirEntry of Deno.readDir(path)) {
+      if (!dirEntry.isFile) {
+        continue;
+      }
+
+      if (!dirEntry.name.endsWith('.book.json')) {
+        continue;
+      }
+
+      const file = await Deno.readTextFile(`${path}/${dirEntry.name}`);
+      const book = JSON.parse(file) as LibraryData;
+
+      library.push(book);
+    }
+
+    return Promise.resolve(library);
+  } catch (e) {
+    console.error(e);
+
+    return Promise.resolve([]);
+  }
+}
--- main.ts
+++ main.ts
@@ -1,28 +1,18 @@
 import { Application } from 'jsr:@oak/oak/application';
 import { Router } from 'jsr:@oak/oak/router';
 import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
-
-// deno-lint-ignore no-explicit-any
-const books = new Map<string, any>();
-books.set('1', {
-  id: '1',
-  title: 'The Hound of the Baskervilles',
-  author: 'Conan Doyle, Arthur',
-});
+import { getLibrary } from './data/library/index.ts';
 
 const router = new Router();
 router
   .get('/', oakCors(), (context) => {
     context.response.body = 'Hello world!';
   })
-  .options('/book', oakCors())
-  .get('/book', oakCors(), (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .get('/book/:id', oakCors(), (context) => {
-    if (books.has(context?.params?.id)) {
-      context.response.body = books.get(context.params.id);
-    }
+  .options('/library', oakCors())
+  .get('/library', oakCors(), async (context) => {
+    const res = await getLibrary('./data/library/books');
+
+    context.response.body = res;
   });
 
 const app = new Application();
